45985c7b2138ecec3f3883830299d36517766184
indgy <65541311+indgy@users.noreply.github.com>
2024-06-27 12:52:18 +0100

    Added faux Generics support

---
+++ src/Generic.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phico;
+
+
+class Generic
+{
+    protected array $data = [];
+    protected bool $readonly = false;
+    protected bool $normalise = false;
+
+
+    public function __construct(array $data = [], bool $readonly = false, bool $normalise = false)
+    {
+        foreach ($data as $k => $v) {
+            if ($this->normalise) {
+                $k = $this->normalise($k);
+            }
+            $this->data[$k] = $v;
+        }
+        $this->readonly = $this->readonly ?? $readonly;
+        $this->normalise = $this->normalise ?? $normalise;
+    }
+    public function __get(string $name): mixed
+    {
+        return $this->get($name);
+    }
+    public function __set(string $name, mixed $value): void
+    {
+        $this->set($name, $value);
+    }
+    public function __isset(string $name): bool
+    {
+        return array_key_exists($name, $this->data);
+    }
+    public function __unset(string $name): void
+    {
+        unset($this->data[$name]);
+    }
+
+    public function all(): array
+    {
+        return $this->data;
+    }
+    public function has(string $prop): bool
+    {
+        if ($this->normalise) {
+            $prop = $this->normalise($prop);
+        }
+        return $this->__isset($prop);
+    }
+    public function get(string $prop, mixed $default = null): mixed
+    {
+        $method = sprintf('get%s', str()->toCamelCase($prop));
+        if (method_exists($this, $method)) {
+            return $this->$method($prop, $default);
+        }
+
+        if ($this->normalise) {
+            $prop = $this->normalise($prop);
+        }
+        return ($this->__isset($prop)) ? $this->data[$prop] : $default;
+    }
+    public function set(string $prop, mixed $value): mixed
+    {
+        if ($this->readonly) {
+            throw new \BadMethodCallException(sprintf("'Cannot set property '%s' on class '%s' as it is readonly", $prop, __CLASS__));
+        }
+
+        $method = sprintf('set%s', str()->toCamelCase($prop));
+        if (method_exists($this, $method)) {
+            $this->$method($value);
+        }
+
+        if ($this->normalise) {
+            $prop = $this->normalise($prop);
+        }
+        $this->data[$prop] = $value;
+        return $this;
+    }
+
+    private function normalise(string $name): string
+    {
+        return str_replace('-', '_', strtolower($name));
+    }
+}
---
+++ src/Generics.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phico;
+
+
+// holds a list of Generic instances
+class Generics implements \Iterator
+{
+    private $position = 0;
+    private array $items = [];
+
+
+    public function __construct()
+    {
+        $this->position = 0;
+    }
+
+    public function all(): array
+    {
+        return $this->items;
+    }
+
+    public function add(Generic $item): self
+    {
+        $this->items[] = $item;
+        return $this;
+    }
+    public function find(): array
+    {
+
+    }
+    public function map(): array
+    {
+
+    }
+    public function match(array $matches): array
+    {
+        // set output array
+        $out = [];
+        // get total number of properties to match on
+        $count = count($matches);
+
+        foreach ($this->items as $item) {
+            $matched = 0;
+            foreach ($matches as $k => $v) {
+                if ($item->$k == $v) {
+                    $matched++;
+                }
+            }
+
+            // number of matches equals the number of props needing to be matched
+            if ($matched == $count) {
+                $out[] = $item;
+            }
+        }
+
+        return $out;
+    }
+    public function reduce(): array
+    {
+
+    }
+
+
+
+    public function rewind(): void
+    {
+        $this->position = 0;
+    }
+
+    #[\ReturnTypeWillChange]
+    public function current()
+    {
+        return $this->item[$this->position];
+    }
+
+    #[\ReturnTypeWillChange]
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next(): void
+    {
+        ++$this->position;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->item[$this->position]);
+    }
+}
